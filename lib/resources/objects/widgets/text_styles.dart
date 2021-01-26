a4c0837d5a38548039a5c992918ccc7af064d9a1
gniewko97 <jan97@onet.eu>
2021-01-26 20:10:35 +0100

    change names and borders in marker forms

--- lib/resources/objects/widgets/text_styles.dart
+++ lib/resources/objects/widgets/text_styles.dart
@@ -19,10 +19,10 @@ TextStyle bodyText({Color color}) {
 
 TextStyle formButtonText({Color color}) {
   if (color != null) {
-    return TextStyle(fontSize: 12, color: color);
+    return TextStyle(fontSize: 14, color: color);
   } else {
     return TextStyle(
-      fontSize: 12,
+      fontSize: 14,
     );
   }
 }
@@ -95,7 +95,7 @@ InputDecoration textFieldStyle({
 
 BoxDecoration buttonFieldStyle() {
   return BoxDecoration(
-    border: Border.all(width: 0.5),
+    border: Border.all(width: 1, color: Colors.grey ),
     borderRadius: BorderRadius.circular(10),
   );
 }
